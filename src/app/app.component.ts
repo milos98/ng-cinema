4d245678d1e558242285c0944e446538af20a7e6
Miloš Janković <mjankovic180@gmail.com>
2020-08-13 00:11:09 +0200

    instance of movie service on app level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { MovieService } from './movie.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MovieService]
 })
 export class AppComponent {
   title = 'ng-cinema';
