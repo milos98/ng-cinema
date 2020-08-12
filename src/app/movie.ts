70bcb9300a1dd033a6ffe796cbf3458fed907903
Miloš Janković <mjankovic180@gmail.com>
2020-08-12 02:41:54 +0200

    created movie intefrace

---
+++ src/app/movie.ts
@@ -0,0 +1,14 @@
+export interface Movie {
+  _id: string;
+  title: string;
+  image: string;
+  genre: string;
+  language: string;
+  duration: number;
+  description: string;
+  director: string;
+  cast: string;
+  __v: number;
+  endDate: string;
+  releaseDate: string;
+}
