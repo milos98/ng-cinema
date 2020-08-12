53a0b3452b5a34a12b918f39f2056c6306602537
Miloš Janković <mjankovic180@gmail.com>
2020-08-12 19:51:59 +0200

    added ngx-slick-carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HttpClientModule} from '@angular/common/http';
-import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { MovieShowComponent } from './movie-show/movie-show.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavbarComponent,
     FooterComponent,
-    MovieShowComponent
+    HomeComponent,
+    MovieShowComponent,
+    NavbarComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
