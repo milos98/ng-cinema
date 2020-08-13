729595fe59f725b478f48403df45462b3eb44253
Miloš Janković <mjankovic180@gmail.com>
2020-08-13 02:30:44 +0200

    updated home component
    added logic for communication with movie service and linked with movie-show and slick-carousel components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MovieService } from './../movie.service';
+import { Category } from './../category';
+import { Movie } from './../movie'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    const currentDate = new Date();
+    this.movieService.getMovies().subscribe(data => {
+      let available: Category = {name:'Now Showing', data: []};
+      let soon: Category = {name:'Comming Soon', data: []};
+
+      data.forEach(movie => {
+        const movieReleaseDate = new Date(movie.releaseDate);
+
+        if (movieReleaseDate > currentDate) {
+          available.data.push(movie);
+        }
+        else {
+          soon.data.push(movie);
+        }
+      });
+
+      this.categories.push(available, soon);
+      console.log(this.categories);
+    });
   }
 
 }
